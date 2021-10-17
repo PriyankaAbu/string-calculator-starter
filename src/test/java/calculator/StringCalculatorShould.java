1325486fe1d334076539f3c9ec0cce7713c36c0a
PriyankaAbu <84837563+PriyankaAbu@users.noreply.github.com>
2021-10-17 22:36:20 +0530

    Update StringCalculatorShould.java

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -11,6 +11,19 @@ class StringCalculatorShould {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
+    public static final int add(final String numbers) { // Changed void to int
+    String[] numbersArray = numbers.split(",");
+    if (numbersArray.length > 2) {
+        throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
+    } else {
+        for (String number : numbersArray) {
+            if (!number.isEmpty()) {
+                Integer.parseInt(number);
+            }
+        }
+    }
+    return 0; // Added return
+}
 
     @Test
     void string_with_single_number_should_return_number_as_int() {
